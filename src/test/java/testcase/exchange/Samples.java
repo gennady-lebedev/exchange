2e7d5c3136fe1233c4a43085c3ef9b599d7df5d0
Gennady <gennady.lebedev@gmail.com>
2017-06-08 21:20:30 +0400

    add sample tests

--- src/test/java/testcase/exchange/Samples.java
+++ src/test/java/testcase/exchange/Samples.java
@@ -27,4 +27,42 @@ public class Samples {
         assertEquals(new BigDecimal("15.30"), dealService.getOptimalPrice());
         assertEquals(new BigInteger("150"), dealService.getMaxAmount());
     }
+
+    @Test
+    public void multipleOptimalPrices() {
+        DiscreteAuction auction = new DiscreteAuction();
+        auction.addBuyOrder(new Order("10", "100"));
+        auction.addBuyOrder(new Order("20", "100"));
+        auction.addBuyOrder(new Order("30", "100"));
+        auction.addBuyOrder(new Order("40", "100"));
+        auction.addBuyOrder(new Order("50", "100"));
+        auction.addBuyOrder(new Order("60", "100"));
+        auction.addSellOrder(new Order("10", "100"));
+        auction.addSellOrder(new Order("20", "100"));
+        auction.addSellOrder(new Order("30", "100"));
+        auction.addSellOrder(new Order("40", "100"));
+        auction.addSellOrder(new Order("50", "100"));
+        auction.addSellOrder(new Order("60", "100"));
+        DealService dealService = auction.exchange();
+        assertEquals(new BigDecimal("35.00"), dealService.getOptimalPrice());
+        assertEquals(new BigInteger("300"), dealService.getMaxAmount());
+    }
+
+    @Test
+    public void over9000() {
+        DiscreteAuction auction = new DiscreteAuction();
+        for(int i = 0; i < DiscreteAuction.ORDERS_LIMIT;) {
+            BigDecimal price = BigDecimal.ONE;
+            BigDecimal inc = new BigDecimal("0.01");
+            String amount = "1000";
+            for(int j = 0; j < 9900 && i < DiscreteAuction.ORDERS_LIMIT; j++,i+=2) {
+                price = price.add(inc);
+                auction.addBuyOrder(new Order(price.toString(), amount));
+                auction.addSellOrder(new Order(price.toString(), amount));
+            }
+        }
+        DealService dealService = auction.exchange();
+        assertEquals(new BigDecimal("50.02"), dealService.getOptimalPrice());
+        assertEquals(new BigInteger("250002000"), dealService.getMaxAmount());
+    }
 }
