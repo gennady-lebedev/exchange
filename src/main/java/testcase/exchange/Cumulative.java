153bcea90dae69d69317ab16132185ae2453d094
Gennady <gennady.lebedev@gmail.com>
2017-06-08 22:36:56 +0400

    refactor add Cumulative class

---
+++ src/main/java/testcase/exchange/Cumulative.java
@@ -0,0 +1,62 @@
+package testcase.exchange;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+@Getter
+@ToString
+public class Cumulative {
+    private static final Logger log = LoggerFactory.getLogger(Cumulative.class);
+
+    private final NavigableMap<BigDecimal, BigInteger> buyCumulative;
+    private final NavigableMap<BigDecimal, BigInteger> sellCumulative;
+
+    public Cumulative(NavigableMap<BigDecimal, BigInteger> buyOrders,
+                      NavigableMap<BigDecimal, BigInteger> sellOrders) {
+        buyCumulative = accumulateBuyOrders(buyOrders);
+        sellCumulative = accumulateSellOrders(sellOrders);
+
+        log.trace("Sell orders grouped by price: {}", sellOrders);
+        log.trace("Sell cumulative: {}", sellCumulative);
+
+        log.trace("Buy orders grouped by price: {}", buyOrders);
+        log.trace("Buy cumulative: {}", buyCumulative);
+    }
+
+    private NavigableMap<BigDecimal, BigInteger> accumulateBuyOrders(NavigableMap<BigDecimal, BigInteger> buyOrders) {
+        NavigableMap<BigDecimal, BigInteger> buyCumulative = new TreeMap<>();
+        for(BigDecimal price: buyOrders.descendingKeySet()) {
+            BigInteger amount = buyOrders.get(price);
+            BigDecimal tailPrice = buyOrders.higherKey(price);
+            if(tailPrice != null) {
+                BigInteger tailAmount = buyCumulative.get(tailPrice);
+                buyCumulative.put(price, amount.add(tailAmount));
+            } else {
+                buyCumulative.put(price, amount);
+            }
+        }
+        return buyCumulative;
+    }
+
+    private NavigableMap<BigDecimal, BigInteger> accumulateSellOrders(NavigableMap<BigDecimal, BigInteger> sellOrders) {
+        NavigableMap<BigDecimal, BigInteger> sellCumulative = new TreeMap<>();
+        for(BigDecimal price: sellOrders.navigableKeySet()) {
+            BigInteger amount = sellOrders.get(price);
+            BigDecimal headPrice = sellOrders.lowerKey(price);
+            if(headPrice != null) {
+                BigInteger headAmount = sellCumulative.get(headPrice);
+                sellCumulative.put(price, amount.add(headAmount));
+            } else {
+                sellCumulative.put(price, amount);
+            }
+        }
+        return sellCumulative;
+    }
+}
